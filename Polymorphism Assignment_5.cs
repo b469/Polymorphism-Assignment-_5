a8d7bfb06722eaa4c58b67cf658bc4a1e826a3ce
b469 <69499965+b469@users.noreply.github.com>
2026-03-29 16:30:01 -0700

    Add files via upload

---
+++ Polymorphism Assignment_5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Abstract_Class_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           IQuittable quittable = new Employee(); // Create an object of the Employee class and have it inherit from the IQuittable interface
+            quittable.Quittable(quittable); // Call the method Quittable on the object of Employee
+
+
+
+
+        }
+
+    
+
+    }
+}
+
+
+ 
+        
+    
+
+    
+
